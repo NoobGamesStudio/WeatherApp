b05669bd99f0c8a0f750e25c582ee6c27e8511da
Dalitrox <79969896+Dalitrox@users.noreply.github.com>
2023-01-18 10:16:33 +0100

    Update WeatherCodes.cs

--- WeatherApp/WA-WeatherAPI/Service/WeatherCodes.cs
+++ WeatherApp/WA-WeatherAPI/Service/WeatherCodes.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace WA_WeatherAPI.Service
 {
     Dictionary<int, string> weatherecode = new()

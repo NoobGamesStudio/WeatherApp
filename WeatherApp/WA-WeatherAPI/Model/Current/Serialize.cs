780f11716b9862bacfc6c191f2ed4f5f56dd21ed
Dalitrox <79969896+Dalitrox@users.noreply.github.com>
2023-01-18 10:12:50 +0100

    Delete Serialize.cs
    Usuwanie zbędnego pliku

--- WeatherApp/WA-WeatherAPI/Model/Current/Serialize.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json;
-
-namespace WA_WeatherAPI.Model.Current
-{
-    public static class Serialize
-    {
-        public static string ToJson(this Current self) => JsonSerializer.Serialize(self, Converter.Settings);
-    }
-}

f8c4f4e325c67c878d2b234f5e4d0fbc420d1fee
Dalitrox <79969896+Dalitrox@users.noreply.github.com>
2023-01-18 23:46:19 +0100

    Dalitrox (#14)
    * Update README.md
    
    * Update README.md
    
    * Parametry na aktualna, godzinną i dzienną pogodę
    
    Zostały wstępnie wprowadzone klasy do serializowacji i deserializacji danych do aplikacji pogodowej. Nadal mogą być błędy zwłaszcza, że było to przerabiane z Newtonsoft.Json na System.Text.Json więc na pewno będzie wymagało sprawdzenia.
    
    * HTTP Client i WeatherCodes enum
    
    Wstępne przygotowanie klienta HTTP oraz przygotowanie enuma do kodów pogodowych. Z nimi chyba będę potrzebował małej pomocy. Nie jestem pewny czy klient HTTP będzie działał poprawnie podobnie jak z serializacją i deserializacją JSONa więc to będzie trzeba jakoś przetestować. Wprowadziłem też małe poprawki do modeli dla poszczególnych rodzajów prognozy żeby nie było czegoś w rodzaju Hourlyhourly zamieniałem na HourlyWeather etc.
    
    * Update WeatherCodes.cs
    
    Zamiana enum na dictionary
    
    * Delete Serialize.cs
    
    Usuwanie zbędnego pliku
    
    * Delete Converter.cs
    
    * Update Current.cs
    
    * Delete Serialize.cs
    
    * Delete Converter.cs
    
    * Update Daily.cs
    
    * Delete Converter.cs
    
    * Delete Serialize.cs
    
    * Update Hourly.cs
    
    * Update WeatherCodes.cs
    
    * Poprawiona klasa do odbierania prognozy pogody
    
    Poza poprawkami klasy Forecast poprawiono również pliki związane z modelami prognoz oraz dodanie odpowiednich usingów do pliku GlobalUsings

---
+++ WeatherApp/WA-Utility/Service/Localization.cs
@@ -0,0 +1,12 @@
+﻿namespace WA_Utility.Service;
+
+public class Localization
+{
+    public string City { get; set; } = "Szczecin";
+    public Uri Uri => new Uri($"https://geocoding-api.open-meteo.com/v1/search?name={City}&count=1");
+    public async Task<Result?> Data() => (await _client.GetFromJsonAsync<Root>(Uri))?.results?[0];
+    
+    HttpClient _client = new HttpClient();
+
+    ~Localization() => _client.Dispose();
+}

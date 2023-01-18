57435f6973757fb85a52ec21b8f6eba953cfd3ba
Dalitrox <79969896+Dalitrox@users.noreply.github.com>
2023-01-18 10:14:58 +0100

    Delete Serialize.cs

--- WeatherApp/WA-WeatherAPI/Model/Hourly/Serialize.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json;
-
-namespace WA_WeatherAPI.Model.Hourly
-{
-    public static class Serialize
-    {
-        public static string ToJson(this Hourly self) => JsonSerializer.Serialize(self, Converter.Settings);
-    }
-}

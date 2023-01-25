8ec2f465e5f89d92bdbbdeb4e9fe882f56e479c3
Dalitrox <79969896+Dalitrox@users.noreply.github.com>
2023-01-25 21:22:14 +0100

    Update Hourly.cs

--- WeatherApp/WA-WeatherAPI/Model/Hourly/Hourly.cs
+++ WeatherApp/WA-WeatherAPI/Model/Hourly/Hourly.cs
@@ -18,7 +18,7 @@ public record Hourly
     public HourlyData? Data { get; init; }
 }
 
-public record HourlyData
+public record HourlyData : ICastable<HourlyDataModel>
 {
     [JsonPropertyName("time")]
     public List<string>? Time { get; init; }
@@ -46,34 +46,104 @@ public record HourlyData
 
     [JsonPropertyName("visibility")]
     public List<float>? Visibility { get; init; }
+
+    public List<HourlyDataModel> Cast()
+    {
+        var res = new List<HourlyDataModel>();
+
+        for (int i = 0; i < Time.Count; i++)
+        {
+            res.Add(new HourlyDataModel
+            {
+                Time = Time[i],
+                Temperature2M = Temperature2M[i],
+                Relativehumidity2M = Relativehumidity2M[i],
+                ApparentTemperature = ApparentTemperature[i],
+                Precipitation = Precipitation[i],
+                Weathercode = Weathercode[i],
+                SurfacePressure = SurfacePressure[i],
+                Cloudcover = Cloudcover[i],
+                Visibility = Visibility[i]
+            });
+        }
+        return res;
+    }
 }
 
-public record HourlyUnits
+public record HourlyDataModel
+{
+    public string? Time { get; init; }
+    public double? Temperature2M { get; init; }
+    public long? Relativehumidity2M { get; init; }
+    public double? ApparentTemperature { get; init; }
+    public double? Precipitation { get; init; }
+    public long? Weathercode { get; init; }
+    public double? SurfacePressure { get; init; }
+    public long? Cloudcover { get; init; }
+    public float? Visibility { get; init; }
+}
+
+public record HourlyUnits : ICastable<HourlyUnitsModel>
 {
     [JsonPropertyName("time")]
-    public string? Time { get; init; }
+    public List<string?> Time { get; init; }
 
     [JsonPropertyName("temperature_2m")]
-    public string? Temperature2M { get; init; }
+    public List<string?> Temperature2M { get; init; }
 
     [JsonPropertyName("relativehumidity_2m")]
-    public string? Relativehumidity2M { get; init; }
+    public List<string?> Relativehumidity2M { get; init; }
 
     [JsonPropertyName("apparent_temperature")]
-    public string? ApparentTemperature { get; init; }
+    public List<string?> ApparentTemperature { get; init; }
 
     [JsonPropertyName("precipitation")]
-    public string? Precipitation { get; init; }
+    public List<string?> Precipitation { get; init; }
 
     [JsonPropertyName("weathercode")]
-    public string? Weathercode { get; init; }
+    public List<string?> Weathercode { get; init; }
 
     [JsonPropertyName("surface_pressure")]
-    public string? SurfacePressure { get; init; }
+    public List<string?> SurfacePressure { get; init; }
 
     [JsonPropertyName("cloudcover")]
-    public string? Cloudcover { get; init; }
+    public List<string?> Cloudcover { get; init; }
 
     [JsonPropertyName("visibility")]
+    public List<string?> Visibility { get; init; }
+
+    public List<HourlyUnitsModel> Cast()
+    {
+        var res = new List<HourlyUnitsModel>();
+
+        for (int i = 0; i < Time.Count; i++)
+        {
+            res.Add(new HourlyUnitsModel
+            {
+                Time = Time[i],
+                Temperature2M = Temperature2M[i],
+                Relativehumidity2M = Relativehumidity2M[i],
+                ApparentTemperature = ApparentTemperature[i],
+                Precipitation = Precipitation[i],
+                Weathercode = Weathercode[i],
+                SurfacePressure = SurfacePressure[i],
+                Cloudcover = Cloudcover[i],
+                Visibility = Visibility[i]
+            });
+        }
+        return res;
+    }
+}
+
+public record HourlyUnitsModel
+{
+    public string? Time { get; init; }
+    public string? Temperature2M { get; init; }
+    public string? Relativehumidity2M { get; init; }
+    public string? ApparentTemperature { get; init; }
+    public string? Precipitation { get; init; }
+    public string? Weathercode { get; init; }
+    public string? SurfacePressure { get; init; }
+    public string? Cloudcover { get; init; }
     public string? Visibility { get; init; }
 }

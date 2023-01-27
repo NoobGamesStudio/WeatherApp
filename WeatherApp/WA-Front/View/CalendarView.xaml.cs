f8503f22ced52feebe8c57b44ccce65c16d62a6f
CJ <78097248+FunnyPaper@users.noreply.github.com>
2023-01-27 19:12:48 +0100

    Front/controls (#29) (#30)
    * update (#27)
    
    * Update README.md
    
    * Update README.md
    
    * Parametry na aktualna, godzinną i dzienną pogodę
    
    Zostały wstępnie wprowadzone klasy do serializowacji i deserializacji danych do aplikacji pogodowej. Nadal mogą być błędy zwłaszcza, że było to przerabiane z Newtonsoft.Json na System.Text.Json więc na pewno będzie wymagało sprawdzenia.
    
    * HTTP Client i WeatherCodes enum
    
    Wstępne przygotowanie klienta HTTP oraz przygotowanie enuma do kodów pogodowych. Z nimi chyba będę potrzebował małej pomocy. Nie jestem pewny czy klient HTTP będzie działał poprawnie podobnie jak z serializacją i deserializacją JSONa więc to będzie trzeba jakoś przetestować. Wprowadziłem też małe poprawki do modeli dla poszczególnych rodzajów prognozy żeby nie było czegoś w rodzaju Hourlyhourly zamieniałem na HourlyWeather etc.
    
    * Update WeatherCodes.cs
    
    Zamiana enum na dictionary
    
    * Delete Serialize.cs
    
    Usuwanie zbędnego pliku
    
    * Delete Converter.cs
    
    * Update Current.cs
    
    * Delete Serialize.cs
    
    * Delete Converter.cs
    
    * Update Daily.cs
    
    * Delete Converter.cs
    
    * Delete Serialize.cs
    
    * Update Hourly.cs
    
    * Update WeatherCodes.cs
    
    * Poprawiona klasa do odbierania prognozy pogody
    
    Poza poprawkami klasy Forecast poprawiono również pliki związane z modelami prognoz oraz dodanie odpowiednich usingów do pliku GlobalUsings
    
    * Dalitrox (#26)
    
    * Aktualne zmiany z maina na mój branch (#25)
    
    * Dalitrox (#14)
    
    * Update README.md
    
    * Update README.md
    
    * Parametry na aktualna, godzinną i dzienną pogodę
    
    Zostały wstępnie wprowadzone klasy do serializowacji i deserializacji danych do aplikacji pogodowej. Nadal mogą być błędy zwłaszcza, że było to przerabiane z Newtonsoft.Json na System.Text.Json więc na pewno będzie wymagało sprawdzenia.
    
    * HTTP Client i WeatherCodes enum
    
    Wstępne przygotowanie klienta HTTP oraz przygotowanie enuma do kodów pogodowych. Z nimi chyba będę potrzebował małej pomocy. Nie jestem pewny czy klient HTTP będzie działał poprawnie podobnie jak z serializacją i deserializacją JSONa więc to będzie trzeba jakoś przetestować. Wprowadziłem też małe poprawki do modeli dla poszczególnych rodzajów prognozy żeby nie było czegoś w rodzaju Hourlyhourly zamieniałem na HourlyWeather etc.
    
    * Update WeatherCodes.cs
    
    Zamiana enum na dictionary
    
    * Delete Serialize.cs
    
    Usuwanie zbędnego pliku
    
    * Delete Converter.cs
    
    * Update Current.cs
    
    * Delete Serialize.cs
    
    * Delete Converter.cs
    
    * Update Daily.cs
    
    * Delete Converter.cs
    
    * Delete Serialize.cs
    
    * Update Hourly.cs
    
    * Update WeatherCodes.cs
    
    * Poprawiona klasa do odbierania prognozy pogody
    
    Poza poprawkami klasy Forecast poprawiono również pliki związane z modelami prognoz oraz dodanie odpowiednich usingów do pliku GlobalUsings
    
    * Front/general (#15-forwarding)
    
    * Shell navigation attempts
    
    * Disabled default title bar for windows
    
    * Layout first attempt
    
    * Views renames, style adjustement, unused files cleanup, viewmodels provided by static property of maui app, prevent user from manual window resizing on windows, new custom expander control
    
    * New layout added, expander fixes
    
    * small fixes
    
    * Update Hourly.cs
    
    * Update Daily.cs
    
    * Delete Converter.cs
    
    * Delete Serialize.cs
    
    * Delete Converter.cs
    
    * Delete Serialize.cs
    
    * Delete Converter.cs
    
    * Delete Serialize.cs
    
    
    
    
    * Dodanie interfejsu oraz dodanie domyślnej lokalizacji jako Szczecin w nazwie miasta
    
    
    
    
    
    
    
    * Views trigger their expanders in different ways
    
    ---------
    
    Co-authored-by: Dalitrox <daniel.ant.ch@gmail.com>
    Co-authored-by: Dalitrox <79969896+Dalitrox@users.noreply.github.com>

--- WeatherApp/WA-Front/View/CalendarView.xaml.cs
+++ WeatherApp/WA-Front/View/CalendarView.xaml.cs
@@ -4,6 +4,8 @@ namespace WA_Front.View;
 
 public partial class CalendarView : ContentView
 {
+	RadioButton selected = null;
+
 	public CalendarView()
 	{
 		InitializeComponent();
@@ -11,4 +13,17 @@ public partial class CalendarView : ContentView
 	}
 
     public override string ToString() => "Calendar";
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+        RadioButton button = e.Parameter as RadioButton;
+		Content.BindingContext = button.BindingContext;
+
+        if (selected == button)
+		{
+			button.IsChecked = !button.IsChecked;
+		}
+	
+        selected = button.IsChecked ? button : null;
+    }
 }

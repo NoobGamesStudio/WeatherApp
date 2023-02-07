eee0a09618e50004984c28d577c4d5e690433314
Dalitrox <daniel.ant.ch@gmail.com>
2023-02-07 19:39:04 +0100

    Testy potwierdzające poprawność odwołania do API

--- WeatherApp/WA-UnitTests/WA_UtilityTests.cs
+++ WeatherApp/WA-UnitTests/WA_UtilityTests.cs
@@ -12,4 +12,14 @@ public class WA_UtilityTests
         ExampleService example = new();
         Assert.True(example.Value == value);
     }
+
+    [Fact]
+    public async void LocalizationTest()
+    {
+        using HttpClient _client = new();
+        Localization localization = new();
+
+        using HttpResponseMessage response = await _client.GetAsync($"https://geocoding-api.open-meteo.com/v1/search?name={localization.City}&count=1");
+        Assert.True(response.IsSuccessStatusCode);
+    }
 }
--- WeatherApp/WA-UnitTests/WA_WeatherAPITests.cs
+++ WeatherApp/WA-UnitTests/WA_WeatherAPITests.cs
@@ -1,4 +1,7 @@
-﻿using WA_WeatherAPI.Model;
+﻿using Microsoft.Maui.Controls;
+using System.Globalization;
+using WA_Utility.Service;
+using WA_WeatherAPI.Model;
 using WA_WeatherAPI.Service;
 
 namespace WA_UnitTests;
@@ -9,7 +12,47 @@ public class WA_WeatherAPITests
     public void ExampleTest()
     {
         string value = "*Castowanie zaklęcia*";
-        ExampleService example = new();
+        WA_WeatherAPI.Service.ExampleService example = new();
         Assert.True(example.Value == value);
+    }
+
+    [Fact]
+    public async void Current()
+    {
+        using HttpClient _client = new();
+        Localization localization = new();
+        Forecast forecast = new(localization);
+
+        (double lat, double lon) = (await localization.Data());
+        using HttpResponseMessage response = await _client.GetAsync($"https://api.open-meteo.com/v1/forecast?latitude={lat.ToString(CultureInfo.InvariantCulture)}&longitude={lon.ToString(CultureInfo.InvariantCulture)}&timezone=auto&current_weather=true");
+        Assert.True(response.IsSuccessStatusCode);
+    }
+
+    [Fact]
+    public async void Hourly()
+    {
+        using HttpClient _client = new();
+        Localization localization = new();
+        Forecast forecast = new(localization);
+        int beforedays = 0;
+        int afterdays = 14;
+
+        (double lat, double lon) = (await localization.Data());
+        using HttpResponseMessage response = await _client.GetAsync($"https://api.open-meteo.com/v1/forecast?latitude={lat.ToString(CultureInfo.InvariantCulture)}&longitude={lon.ToString(CultureInfo.InvariantCulture)}&timezone=auto&start_date={DateTime.UtcNow.AddDays(beforedays).ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}&end_date={DateTime.UtcNow.AddDays(afterdays).ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,weathercode,surface_pressure,cloudcover,visibility");
+        Assert.True(response.IsSuccessStatusCode);
+    }
+
+    [Fact]
+    public async void Daily()
+    {
+        using HttpClient _client = new();
+        Localization localization = new();
+        Forecast forecast = new(localization);
+        int beforedays = -14;
+        int afterdays = 14;
+
+        (double lat, double lon) = (await localization.Data());
+        using HttpResponseMessage response = await _client.GetAsync($"https://api.open-meteo.com/v1/forecast?latitude={lat.ToString(CultureInfo.InvariantCulture)}&longitude={lon.ToString(CultureInfo.InvariantCulture)}&timezone=auto&start_date={DateTime.UtcNow.AddDays(beforedays).ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}&end_date={DateTime.UtcNow.AddDays(afterdays).ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum");
+        Assert.True(response.IsSuccessStatusCode);
     }
 }
